92be46124afc8992185844767c4ccf262c7c91eb
Xaria Kirtikar <xariak91@gmail.com>
2019-05-21 06:37:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,5 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string)
+  if (string === "I love you, Grandma.") {
+    return 'I love you, too.';
+  }
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!";
+  }
+  if (string === string.toUpperCase()) {
+    return 'YES INDEED!';
+  }
 }
